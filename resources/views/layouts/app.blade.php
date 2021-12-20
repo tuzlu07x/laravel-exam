03ed9b1c3e773fe38c50412d428ab5c328ff0ff9
tuzlu07x <fatihtuzlu07@gmail.com>
2021-12-20 17:02:44 +0300

    düzeltmeler.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Role') }}</title>
+    <title>{{ $title ?? 'Quiz' }} - {{ config('app.name','Laravel') }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
